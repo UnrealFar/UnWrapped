1eeb4115f0da7ae74d4d8c04d37cfe85cc14c061
UnrealFar <ps4rtnite1234@gmail.com>
2024-10-02 09:41:52 +0530

    Inital Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+class App(FastAPI):
+    ...
+
+app = App(
+    title="UnWrap",
+    description="Get your spotify unwrapped any time of the year(You can modify it too!)"
+)
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+uvicorn.run(app, host="0.0.0.0", port=1602)
